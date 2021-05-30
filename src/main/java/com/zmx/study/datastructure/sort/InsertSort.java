3f2c4e706b901d5036b3be533c1dfb7f4005b907
zhangmaoxin <497122067@qq.com>
2021-05-30 16:03:26 +0800

    排序：插入排序

---
+++ src/main/java/com/zmx/study/datastructure/sort/InsertSort.java
@@ -0,0 +1,37 @@
+package com.zmx.study.datastructure.sort;
+
+import java.util.Arrays;
+
+public class InsertSort {
+    public static void main(String[] args) {
+        int[] arr = new int[]{15, 3, 9, -1, 10, 12, 5};
+
+        insertSort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+
+    private static void insertSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            // 1.定义待插入的数
+            int insertValue = arr[i];
+            // 2.定义待插入的索引（当前数的前一个数）
+            int insertIndex = i - 1;
+
+            // 3.当待插入的数字比前一个索引位置的数字小，则待插入索引位置的数字后移，直到找到要插入的位置
+            while (insertIndex >= 0 && insertValue < arr[insertIndex]) {
+                arr[insertIndex + 1] = arr[insertIndex];
+                insertIndex--;
+            }
+
+             // 4.已经找到待插入位置
+            if (insertIndex + 1 != i) {
+                arr[insertIndex + 1] = insertValue;
+            }
+
+            System.out.println("num=" + insertValue);
+            System.out.println("index=" + insertIndex);
+        }
+    }
+}
--- src/main/java/com/zmx/study/datastructure/sort/SelectorSort.java
+++ src/main/java/com/zmx/study/datastructure/sort/SelectorSort.java
@@ -12,7 +12,7 @@ public class SelectorSort {
         for (int i = 0; i < arr.length; i++) {
             int min = arr[i];
             int minIndex = i;
-            for (int j = i; j < arr.length; j++) {
+            for (int j = i + 1; j < arr.length; j++) {
                 if (arr[j] < min) {
                     min = arr[j];
                     minIndex = j;
